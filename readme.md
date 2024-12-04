6ccf75d5d115ce1a2cd02b566e0879970f33dfdb
DoniLite <yaomessan13@outlook.com>
2024-12-04 13:05:39 +0000

    first

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Graphics Studio
+
+Ce projet est un petit studio destiné essentiellement à la création et la présentation de mes réalisations graphiques: `logo`, `affiches`
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-require-imports */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{ts,js}', './**/*.html'],
+  theme: {
+    extend: {
+      // vos extensions de thème
+    },
+  },
+  plugins: [],
+};
