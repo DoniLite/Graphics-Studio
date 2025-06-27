2279d7d1f0535682d7f87acf46bcfe901d6d2835
DoniLite <yaomessan13@outlook.com>
2025-06-27 16:53:01 +0000

    refactor: structure organization

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,11 @@
 import puppeteer from 'puppeteer';
+import fs from 'node:fs';
 
-export const downloadImage = async (url: string, logo: string) => {
+export const downloadImage = async (htmlContent: string, logo: string) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
+  const html = fs.readFileSync(htmlContent, {encoding: 'utf-8'});
+  await page.setContent(html);
   const screenshot = await page.screenshot({
     encoding: 'base64',
     type: 'png',
